58ffee6f519915282377ae4a6619b95aee067eab
will_code <will.tech.code@gmail.com>
2026-01-21 18:52:24 -0500

    ready admin app - bugs fixed

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.clienteWeb.app',
-  appName: 'client_web',
-  webDir: 'build'
-};
-
-export default config;
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
 
+
+
 function App() {
   return (
     <Router>
